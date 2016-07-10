3f670f5a3379c2ea4724d32f5c12bbe34c67e5c7
Shota Shimazu <shota-shimazu.eis@keio.jp>
2016-07-10 22:22:19 +0900

    Update source.

--- Deployment/Scripts/main.js
+++ Deployment/Scripts/main.js
@@ -6,7 +6,19 @@ var keybordIDs = [
     "C2", "C2s", "D2", "D2s", "E2", "F2", "F2s", "G2", "G2s", "A3", "A3s", "B3", "C3", "C3s",
     "D3", "D3s", "E3", "F3", "F3s", "G3", "G3s", "A4", "A4s", "B4", "C4", "C4s", "D4", "D4s", "E4"
 ];
-var soundFonts = [
+var classicSoundIDs = [
+    "c2.mp3", "c2s.mp3", "d2.mp3", "d2s.mp3", "e2.mp3", "f2.mp3", "f2s.mp3", "g2.mp3", "g2s.mp3",
+    "a3.mp3", "a3s.mp3", "b3.mp3", "c3.mp3", "c3s.mp3", "d3.mp3", "d3s.mp3", "e3.mp3 ", "f3.mp3",
+    "f3s.mp3", "g3.mp3", "g3s.mp3", "a4.mp3", "a4s.mp3", "b4.mp3", "c4.mp3", "c4s.mp3", "d4.mp3",
+    "d4s.mp3", "e4.mp3"
+];
+var cuteSoundIDs = [
+    "c2.mp3", "c2s.mp3", "d2.mp3", "d2s.mp3", "e2.mp3", "f2.mp3", "f2s.mp3", "g2.mp3", "g2s.mp3",
+    "a3.mp3", "a3s.mp3", "b3.mp3", "c3.mp3", "c3s.mp3", "d3.mp3", "d3s.mp3", "e3.mp3 ", "f3.mp3",
+    "f3s.mp3", "g3.mp3", "g3s.mp3", "a4.mp3", "a4s.mp3", "b4.mp3", "c4.mp3", "c4s.mp3", "d4.mp3",
+    "d4s.mp3", "e4.mp3"
+];
+var rockSoundIDs = [
     "c2.mp3", "c2s.mp3", "d2.mp3", "d2s.mp3", "e2.mp3", "f2.mp3", "f2s.mp3", "g2.mp3", "g2s.mp3",
     "a3.mp3", "a3s.mp3", "b3.mp3", "c3.mp3", "c3s.mp3", "d3.mp3", "d3s.mp3", "e3.mp3 ", "f3.mp3",
     "f3s.mp3", "g3.mp3", "g3s.mp3", "a4.mp3", "a4s.mp3", "b4.mp3", "c4.mp3", "c4s.mp3", "d4.mp3",
@@ -18,6 +30,12 @@ var rockKeyElements = new Array(29);
 function getUrl(sfFile) {
     var baseUrl = "https://web.sfc.keio.ac.jp/~t16440ss/webPiano/SoundFonts/";
     return baseUrl + sfFile;
+}
+function splashSound(i) {
+    alert(i);
+    var SfFile = getUrl(classicSoundIDs[i]);
+    var KeyElms = document.createElement("audio");
+    KeyElms.setAttribute("src", SfFile);
 }
 for (var i = 0; i <= 28; i++) {
     (function (i) {
@@ -37,19 +55,19 @@ window.addEventListener('load', function () {
     for (var i = 0; i <= 28; i++) {
         (function (i) {
             classicKeyElements[i].addEventListener('click', function () {
-                alert(i);
+                splashSound(i);
             }, false);
             return i;
         })(i);
         (function (i) {
             cuteKeyElements[i].addEventListener('click', function () {
-                alert(i);
+                splashSound(i);
             }, false);
             return i;
         })(i);
         (function (i) {
             rockKeyElements[i].addEventListener('click', function () {
-                alert(i);
+                splashSound(i);
             }, false);
             return i;
         })(i);
--- Sources/Scripts/main.ts
+++ Sources/Scripts/main.ts
@@ -9,7 +9,21 @@ let keybordIDs: Array<string> = [
 	 "D3", "D3s", "E3", "F3", "F3s", "G3", "G3s", "A4",  "A4s", "B4", "C4", "C4s", "D4", "D4s", "E4"
 ];
 
-let soundFonts: Array<string> = [
+let classicSoundIDs: Array<string> = [
+ 	"c2.mp3", "c2s.mp3", "d2.mp3", "d2s.mp3", "e2.mp3", "f2.mp3", "f2s.mp3", "g2.mp3", "g2s.mp3",
+	"a3.mp3", "a3s.mp3", "b3.mp3", "c3.mp3", "c3s.mp3", "d3.mp3", "d3s.mp3", "e3.mp3 ", "f3.mp3", 
+	"f3s.mp3", "g3.mp3", "g3s.mp3", "a4.mp3", "a4s.mp3", "b4.mp3", "c4.mp3", "c4s.mp3", "d4.mp3",
+	"d4s.mp3", "e4.mp3"
+];
+
+let cuteSoundIDs: Array<string> = [
+ 	"c2.mp3", "c2s.mp3", "d2.mp3", "d2s.mp3", "e2.mp3", "f2.mp3", "f2s.mp3", "g2.mp3", "g2s.mp3",
+	"a3.mp3", "a3s.mp3", "b3.mp3", "c3.mp3", "c3s.mp3", "d3.mp3", "d3s.mp3", "e3.mp3 ", "f3.mp3", 
+	"f3s.mp3", "g3.mp3", "g3s.mp3", "a4.mp3", "a4s.mp3", "b4.mp3", "c4.mp3", "c4s.mp3", "d4.mp3",
+	"d4s.mp3", "e4.mp3"
+];
+
+let rockSoundIDs: Array<string> = [
  	"c2.mp3", "c2s.mp3", "d2.mp3", "d2s.mp3", "e2.mp3", "f2.mp3", "f2s.mp3", "g2.mp3", "g2s.mp3",
 	"a3.mp3", "a3s.mp3", "b3.mp3", "c3.mp3", "c3s.mp3", "d3.mp3", "d3s.mp3", "e3.mp3 ", "f3.mp3", 
 	"f3s.mp3", "g3.mp3", "g3s.mp3", "a4.mp3", "a4s.mp3", "b4.mp3", "c4.mp3", "c4s.mp3", "d4.mp3",
@@ -29,6 +43,13 @@ function getUrl(sfFile: string): string {
 	return baseUrl + sfFile;
 }
 
+
+function splashSound(i: number): void {
+	alert(i);
+	let SfFile = getUrl(classicSoundIDs[i]);
+	let KeyElms = document.createElement("audio");
+	KeyElms.setAttribute("src", SfFile);
+}
 
 
 
@@ -53,13 +74,17 @@ for (var i: number = 0 ; i <= 28; i++) {
 
 
 
+
+
+
+
 window.addEventListener('load', function (): void {
 		
 	for (var i: number = 0; i <= 28; i++) {
 
 		(function (i): EventListenerObject {
 			classicKeyElements[i].addEventListener('click', function () {
-				alert(i);
+				splashSound(i);
 			}, false);
 			return i;
 		})(i);
@@ -67,15 +92,15 @@ window.addEventListener('load', function (): void {
 
 		(function (i): EventListenerObject {
 			cuteKeyElements[i].addEventListener('click', function () {
-				alert(i);
+				splashSound(i);
 			}, false);
 			return i;
 		})(i);
 
-		
+
 		(function (i): EventListenerObject {
 			rockKeyElements[i].addEventListener('click', function () {
-				alert(i);
+				splashSound(i);
 			}, false);
 			return i;
 		})(i);
